033c2b6eec6e26a26c5a739abca08825e49200d6
viczveras1 <viczveras1@gmail.com>
2022-06-07 20:51:08 -0300

    primeiro código básico xmapp

---
+++ primeiro código html básico/index.php
@@ -0,0 +1,17 @@
+<html>
+
+     <head>
+	     <title> Teste PHP ! ! ! </title>
+	 </head>
+
+     <body>
+
+          <?php 
+              for ( $i = 0 ; $i < 10 ; $i++ ) {
+                   print( "Linha número " . $i . "<br />");
+              }
+           ?> 
+
+	 </body>
+
+</html>	 
