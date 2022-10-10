ad5832bcb9baeae009f8b371e837840effef4038
viczveras1 <viczveras1@gmail.com>
2022-10-10 11:16:37 -0300

    first commit

---
+++ src/eclipsetestrun/eclipsetestrun.java
@@ -0,0 +1,17 @@
+package eclipsetestrun;
+
+import eclipsetestrun.model.Gato;
+
+public class eclipsetestrun {
+
+	public static void main(String[] args) {
+		// gerado automaticamente pela IDE.
+        Gato gato = new Gato(null, null, null);
+		int a = 2;
+		int b = 2;
+		System.out.println("ellen da o butao");
+	
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author victo
+ *
+ */
+module eclipsetestrun {
+}
